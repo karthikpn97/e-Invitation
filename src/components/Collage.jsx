639868781218af84f8cd70d31d5e0a429a35437b
Nanda Nithin <71000223+nithinmk26@users.noreply.github.com>
2025-11-30 18:54:36 +0530

    Remove unused image imports
    Removed unused image imports from Collage component.

--- src/components/Collage.jsx
+++ src/components/Collage.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import img1 from '../assets/image1.jpg';
-import img2 from '../assets/image2.jpg';
 import img3 from '../assets/image3.jpg';
 import img4 from '../assets/image4.jpg';
 
 const images = [
-  img1,
-  img2,
   img3,
   img4
 ];
